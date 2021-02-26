45f330e99a2c455e6c337496b8dc9de57b1253b7
VladShev74 <vladadshev@gmail.com>
2021-02-26 23:06:40 +0200

    done homework7

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Profile from './Profile';
+
+const user = {
+    name: "Jacques Gluke",
+    tag: "jgluke",
+    location: "Ocho Rios, Jamaica",
+    avatar: "https://www.flaticon.com/svg/static/icons/svg/3784/3784184.svg",
+    stats: {
+      followers: 5603,
+      views: 4827,
+      likes: 1308
+    }
+}
+
+export default function App() {
+    return (
+        <div>
+            <Profile 
+            user={user}
+            />
+        </div>
+)};
